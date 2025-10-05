a4857c474735dee399c0c7b9e37e375c490423ab
Nerixyz <nerixdev@outlook.de>
2025-10-05 15:08:44 +0200

    update macos

--- pwsh/get-vcpkg-deps.ps1
+++ pwsh/get-vcpkg-deps.ps1
@@ -95,6 +95,7 @@ function WriteOverlayTriplet() {
 function InstallPackages() {
     WriteOverlayTriplet
 
+    & "$env:VCPKG_ROOT/$vcpkgexec" libavif[dav1d]
     & "$env:VCPKG_ROOT/$vcpkgexec" install libjxl libavif[dav1d] libheif openexr zlib libraw
 }
 
